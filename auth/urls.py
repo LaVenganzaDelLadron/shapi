8b0b1e2887fc8b9ef378188f9fa4f2701ba8e256
LaVenganzaDelLadron <darkglitch5417@gmail.com>
2026-03-01 01:02:53 +0800

    updating the hosts

--- auth/urls.py
+++ auth/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .controllers.auth_controller import LoginController, LogoutController, SignupController, CSRFTokenView
+from .controllers.auth_controller import LoginController, LogoutController, SignupController
 
 urlpatterns = [
     path('signup/', SignupController.as_view(), name='signup'),
     path('login/', LoginController.as_view(), name='login'),
-    path('csrf/',CSRFTokenView.as_view(), name='csrf'),
     path('logout/', LogoutController.as_view(), name='logout'),
 ]
