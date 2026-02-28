e79d35a814a4fd6ca9fefe7b881865bad4973958
LaVenganzaDelLadron <darkglitch5417@gmail.com>
2026-03-01 02:10:44 +0800

    updating the logut

--- growth/models.py
+++ growth/models.py
@@ -5,5 +5,13 @@ class GrowthStage(models.Model):
     growth_name = models.CharField(max_length=120, unique=True)
     date = models.DateTimeField()
 
+    @classmethod
+    def generate_growth_code(cls):
+        prefix = 'GROWTH-'
+
+
+
+
+
     def __str__(self):
         return f'{self.growth_code} {self.growth_name}'
